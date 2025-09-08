d735c105bbe56af4cece9ebd4a455e621c0d9941
LinkingMx <armando.reyes@live.com.mx>
2025-09-08 11:27:53 -0600

    chnages on bg color
    test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
             html {
-                background-color: oklch(1 0 0);
+                background-color: #ffffff;
             }
 
             html.dark {
@@ -35,9 +35,9 @@
 
         <!-- PWA Meta Tags -->
         <link rel="manifest" href="/manifest.json">
-        <meta name="theme-color" content="#2563eb">
+        <meta name="theme-color" content="#ffffff">
         <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="apple-mobile-web-app-title" content="Costeno Sales">
         <meta name="mobile-web-app-capable" content="yes">
 
